a9ac4a9bacce39d4b1df031197fec759934d07d5
nimantha1122 <144886239+nimantha1122@users.noreply.github.com>
2023-09-13 08:42:26 +0100

    Update streamlit_app.py
    Added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣 Omega 3 & Breakfast oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
 streamlit.text('🐔 Hard boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
