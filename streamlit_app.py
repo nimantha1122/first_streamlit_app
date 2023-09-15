1647d6b53349d23c628122aa390783b89a8b6131
nimantha1122 <144886239+nimantha1122@users.noreply.github.com>
2023-09-15 16:36:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,23 +37,23 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-use role pc_rivery_role;
-use warehouse pc_rivery_wh;
+# use role pc_rivery_role;
+# use warehouse pc_rivery_wh;
 
-create or replace TABLE PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST (
-	FRUIT_NAME VARCHAR(25)
-);
+# create or replace TABLE PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST (
+# 	FRUIT_NAME VARCHAR(25)
+# );
 
-insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
-values ('banana')
-, ('cherry')
-, ('strawberry')
-, ('pineapple')
-, ('apple')
-, ('mango')
-, ('coconut')
-, ('plum')
-, ('avocado')
-, ('starfruit');
+# insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
+# values ('banana')
+# , ('cherry')
+# , ('strawberry')
+# , ('pineapple')
+# , ('apple')
+# , ('mango')
+# , ('coconut')
+# , ('plum')
+# , ('avocado')
+# , ('starfruit');
 
 
