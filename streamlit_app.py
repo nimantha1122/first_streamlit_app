fedf36bda97645d75b53c8b48218f31c7bd03806
nimantha1122 <144886239+nimantha1122@users.noreply.github.com>
2023-09-15 14:18:16 +0100

    Update streamlit_app.py
    changed code to show fruitvice get request response as json on app

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
-
+############################# API tutorial on streamlit##################################
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
+# Write json to app
+streamlit.text(fruityvice_response.json())
 
 
