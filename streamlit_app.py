f6fc213585aaa693c62cc6cbcd99e2083bb6c202
nimantha1122 <144886239+nimantha1122@users.noreply.github.com>
2023-09-15 16:28:16 +0100

    Update streamlit_app.py
    removed snowflake code

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,21 +34,3 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Outputs table as a dataframe onto streamlit app UI
 streamlit.dataframe(fruityvice_normalized)
 
-use role pc_rivery_role;
-use warehouse pc_rivery_wh;
-
-create or replace TABLE PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST (
-	FRUIT_NAME VARCHAR(25)
-);
-
-insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
-values ('banana')
-, ('cherry')
-, ('strawberry')
-, ('pineapple')
-, ('apple')
-, ('mango')
-, ('coconut')
-, ('plum')
-, ('avocado')
-, ('starfruit');
