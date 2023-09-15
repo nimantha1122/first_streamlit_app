ff529be994ac705ae1ba849551677d3464707de5
nimantha1122 <144886239+nimantha1122@users.noreply.github.com>
2023-09-15 16:49:42 +0100

    Update streamlit_app.py
    fixed snowflake code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,7 @@ import pandas
 import requests
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favourites')
@@ -63,4 +58,9 @@ values ('banana')
 , ('avocado')
 , ('starfruit');
 
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_row)
