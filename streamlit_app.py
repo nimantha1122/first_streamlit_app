89395c55567dfc919ec8e9a094d32da6265467ae
nimantha1122 <144886239+nimantha1122@users.noreply.github.com>
2023-09-15 16:52:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Outputs table as a dataframe onto streamlit app UI
 streamlit.dataframe(fruityvice_normalized)
 
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 # use role pc_rivery_role;
 # use warehouse pc_rivery_wh;
@@ -58,9 +63,4 @@ streamlit.dataframe(fruityvice_normalized)
 # , ('avocado')
 # , ('starfruit');
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+
