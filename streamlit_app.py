95917d05451f5190571c54dbd4fb595f6907b280
nimantha1122 <144886239+nimantha1122@users.noreply.github.com>
2023-09-26 13:48:52 +0100

    Update streamlit_app.py
    put API get request of fruit data into a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 ############################# API tutorial on streamlit##################################
+
+
 streamlit.header("Fruityvice Fruit Advice!")
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    # Normalize semi-structured JSON data into a flat table.
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruitvice_normalized
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
+    back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # Normalize semi-structured JSON data into a flat table.
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
     # Outputs table as a dataframe onto streamlit app UI
     streamlit.dataframe(fruityvice_normalized)
 
